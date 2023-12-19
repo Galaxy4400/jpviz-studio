b4861557d88e1408ad541f1160f0b255c9f51aac
Moiseev Evgeny <moiseevEO@yandex.ru>
2023-12-19 22:51:48 +0300

    commit

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,10 @@ import Inspect from 'vite-plugin-inspect';
 export default defineConfig({
 	plugins: [
 		laravel({
-			input: ['resources/css/app.css', 'resources/js/app.js'],
+			input: [
+				'resources/scss/style.scss', 
+				'resources/js/app.js'
+			],
 			refresh: true,
 		}),
 		Inspect(),
